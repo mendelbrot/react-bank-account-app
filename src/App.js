8a77d2d60cd8a6401ea7d910e34e868afdbd36fa
Gregory Maxedon <gregory.maxedon@gmail.com>
2019-01-13 21:13:54 -0800

    about to do a refactor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,145 @@
 import React from 'react';
+
 const BASE_URL = 'http://localhost:51146/api/clientaccounts';
+
+const fetchService = {
+  getAll(callback, errorCallback) {
+    const URL = BASE_URL;
+
+    fetch(URL).then(response => response.json())
+      .then((data) => callback(data))
+      .catch((error) => errorCallback(error));
+  },
+
+  create() {
+    const URL = BASE_URL;
+
+    var clientID = this.clientIDCreateInput.value;
+    this.clientIDCreateInput.value = '';
+    var accountID = this.accountIDCreateInput.value;
+    this.accountIDCreateInput.value = '';
+    var balance = this.balanceCreateInput.value;
+    this.balanceCreateInput.value = '';
+
+    fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        'clientID': clientID,
+        'accountID': accountID,
+        'balance': balance
+      })
+    })
+      .then(response => response.json())
+      .then((json) => {
+        console.log(JSON.stringify(json));
+        this.getAll();
+        this.getDetails();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+
+  update(balance) {
+    const URL = BASE_URL
+    fetch(URL, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        'clientID': this.state.detailsItem.clientID,
+        'accountID': this.state.detailsItem.accountID,
+        'balance': balance
+      })
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(JSON.stringify(json));
+        this.getAll();
+        this.getDetails();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+
+  delete(item) {
+    const URL = BASE_URL + '/delete'
+      + '?clientid=' + item.clientID
+      + '&accountid=' + item.accountID;
+    fetch(URL, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(JSON.stringify(json));
+        this.getAll();
+        this.getDetails();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+
+  getDetails() {
+    var clientID = this.clientIDDetailsInput.value;
+    var accountID = this.accountIDDetailsInput.value;
+
+    const URL = BASE_URL + '/getdetails'
+      + '?clientid=' + clientID
+      + '&accountid=' + accountID;
+
+    fetch(URL).then(response => response.json())
+      .then((data) => {
+        this.setState({ detailsItem: data });
+        console.log(JSON.stringify(data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+};
+
+function DetailsView(props) {
+
+  var balanceUpdateInput;
+
+  console.log(props.item);
+
+  if (props.item != null) {
+    return (
+      <div>
+
+        <hr/>
+
+        First Name: {props.item.firstName} <br/>
+        Last Name: {props.item.lastName} <br />
+        Account Description: {props.item.accountDescription} <br />
+        Balance: {props.item.balance} <br />
+
+        <h3>Edit Balance:</h3>
+        Balance:<input type="text" ref={(element) => balanceUpdateInput = element} /><br />
+        <button onClick={() => props.update(balanceUpdateInput.value)}>Update</button>
+
+        <hr/>
+
+      </div>
+    );
+  } else {
+    return null;
+  }
+  
+};
 
 class App extends React.Component {
 
@@ -7,9 +147,9 @@ class App extends React.Component {
     super();
     this.state = {
       clientAccounts: [], 
-      clientAccount: null, 
-      clientAccountDetails: null
+      detailsItem: null
     };
+
     this.getAll = this.getAll.bind(this);
     this.create = this.create.bind(this);
     this.update = this.update.bind(this);
@@ -19,6 +159,37 @@ class App extends React.Component {
 
   componentDidMount() {
     this.getAll();
+  }
+
+  render() {
+    return (
+      <div>
+
+        <h2>Show Client Account Details/Update</h2>
+        Client ID:<input type="text" ref={(element) => this.clientIDDetailsInput = element} /><br />
+        Account ID:<input type="text" ref={(element) => this.accountIDDetailsInput = element} /><br />
+        <button onClick={this.getDetails}>Show/Update</button>
+        <DetailsView item={this.state.detailsItem} update={this.update}/>
+
+        <h2>Create New Client Account</h2>
+        Client ID:<input type="text" ref={(element) => this.clientIDCreateInput = element} /><br />
+        Account ID:<input type="text" ref={(element) => this.accountIDCreateInput = element} /><br />
+        Balance:<input type="text" ref={(element) => this.balanceCreateInput = element} /><br />
+        <button onClick={this.create}>Create</button>
+        
+        <h2>Client Accounts List</h2>
+        <ul>
+          {this.state.clientAccounts.map((item, index) => (
+            <li key={index}>
+              ClientID: {item.clientID}, AccountID: {item.accountID} , Balance: {item.balance}
+              <button onClick={(e) => this.delete(item)}>Show Details/Update</button>
+              <button onClick={(e) => this.delete(item)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+
+      </div>
+    )
   }
 
   getAll() {
@@ -37,12 +208,12 @@ class App extends React.Component {
   create() {
     const URL = BASE_URL;
 
-    var clientID = this.clientIDInput.value;
-    this.clientIDInput.value = '';
-    var accountID = this.accountIDInput.value;
-    this.accountIDInput.value = '';
-    var balance = this.balanceInput.value;
-    this.balanceInput.value = '';
+    var clientID = this.clientIDCreateInput.value;
+    this.clientIDCreateInput.value = '';
+    var accountID = this.accountIDCreateInput.value;
+    this.accountIDCreateInput.value = '';
+    var balance = this.balanceCreateInput.value;
+    this.balanceCreateInput.value = '';
 
     fetch(URL, {
       method: 'POST',
@@ -60,14 +231,36 @@ class App extends React.Component {
       .then((json) => {
         console.log(JSON.stringify(json));
         this.getAll();
+        this.getDetails();
       })
       .catch((error) => {
         console.log(error);
       });
   }
 
-  update() {
-
+  update(balance) {
+    const URL = BASE_URL
+    fetch(URL, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        'clientID': this.state.detailsItem.clientID,
+        'accountID': this.state.detailsItem.accountID,
+        'balance': balance
+      })
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(JSON.stringify(json));
+        this.getAll();
+        this.getDetails();
+      })
+      .catch(function (error) {
+        console.log(error);
+    });
   }
 
   delete(item) {
@@ -85,34 +278,32 @@ class App extends React.Component {
       .then(json => {
         console.log(JSON.stringify(json));
         this.getAll();
+        this.getDetails();
       })
       .catch(function (error) {
         console.log(error);
-      })
+      });
   }
 
   getDetails() {
+    var clientID = this.clientIDDetailsInput.value;
+    var accountID = this.accountIDDetailsInput.value;
 
-  }
+    const URL = BASE_URL + '/getdetails'
+    + '?clientid=' + clientID
+    + '&accountid=' + accountID;
 
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.state.clientAccounts.map((item, index) => (
-            <li key={index}>
-              ClientID: {item.clientID}, AccountID: {item.accountID} , Balance: {item.balance} 
-              <button onClick={(e) => this.delete(item)}>Delete</button> 
-            </li>
-          ))}
-        </ul>
-        <h3>Create New Client Account</h3>
-        Client ID:<input type="text" ref={(element) => this.clientIDInput = element} /><br/>
-        Account ID:<input type="text" ref={(element) => this.accountIDInput = element} /><br />
-        Balance:<input type="text" ref={(element) => this.balanceInput = element} /><br />
-        <button onClick={this.create}>Create</button>
-      </div>
-    )
+    fetch(URL).then(response => response.json())
+      .then((data) => {
+        this.setState({ detailsItem: data });
+        console.log(JSON.stringify(data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
   }
 }
 export default App;
+
+
